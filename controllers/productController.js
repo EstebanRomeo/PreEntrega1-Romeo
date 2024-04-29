a6ff95af6a7af037e71ce377bca9a2e5635a0545
EstebanRomeo <86939894+EstebanRomeo@users.noreply.github.com>
2024-04-29 17:47:32 -0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,23 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../dao/models/productModel');
 
-// Handlers for product routes
+
 router.get('/', async (req, res) => {
-  // Implement logic to retrieve products from the database
+
 });
 
 router.post('/', async (req, res) => {
-  // Implement logic to add a new product to the database
 });
 
 router.put('/:pid', async (req, res) => {
-  // Implement logic to update a product in the database
 });
 
 router.delete('/:pid', async (req, res) => {
-  // Implement logic to delete a product from the database
 });
 
 module.exports = router;
-
-// Other controller logic for home page, real time products, etc.
+.
